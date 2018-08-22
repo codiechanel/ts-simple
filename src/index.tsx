c5cd939cf63af4c8e52e1e2dd89c0b0ca5ae750b
codiechanel <codiechanel@gmail.com>
2018-08-22 11:32:04 +0800

    prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import './index.css'
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import "./index.css"
 
 class App extends React.Component<any, any> {
-    render() {
-        return (<div className="flex-grid">
-            <div className="col">hello</div>
-            <div className="col">coolness</div>
-            <div className="col">great</div>
-        </div>)
-
-    }
+  render() {
+    return (
+      <div className="flex-grid">
+        <div className="col">hello</div>
+        <div className="col">coolness</div>
+        <div className="col">great</div>
+      </div>
+    )
+  }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"))
