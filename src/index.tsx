c3bf6cb0aaa4b0a00b52cd8dc32f2f070d704035
codiechanel <codiechanel@gmail.com>
2018-08-22 11:21:21 +0800

    3 columns

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,12 @@ import './index.css'
 
 class App extends React.Component<any, any> {
     render() {
-        return "hello"
+        return (<div className="flex-grid">
+            <div className="col">hello</div>
+            <div className="col">coolness</div>
+            <div className="col">great</div>
+        </div>)
+
     }
 }
 
